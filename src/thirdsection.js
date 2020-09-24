7785b6907fb4ec1eb0b8a3d164ca6ce6746afd5a
jacoloves <5511068t@gmail.com>
2020-09-24 21:52:49 +0900

    20200924

--- src/thirdsection.js
+++ src/thirdsection.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import './style.css';
+import Fab from '@material-ui/core/Fab';
 
 import Program1 from './images/program_image.jpg'
 
+const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+}));
+
 const ThirdSection = () => {
+    const classes = useStyles();
+
     return (
         <section class="thirdconatainer">
             <div class="thirdconatainer-inner">
@@ -45,10 +55,18 @@ const ThirdSection = () => {
                     </li>   
                 </ul>
                 <div class="thirdcopy-video">
-                <iframe id="player" type="text/html" width="640" height="360"
-                    src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
-                    frameborder="0">
-                </iframe>
+                    <iframe id="player" type="text/html" width="640" height="360"
+                        src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                        frameborder="0">
+                    </iframe>
+                </div>
+                <div class="thirdcopy-font2">
+                    <span class="thirdcopy-em2">プログラムの宣伝などを記載してください。</span>
+                </div>
+                <div class="sample_button">
+                    <Fab variant="extended" color="secondary" aria-label="add" className={classes.margin}>
+                        好きな文言を入れてください
+                    </Fab>
                 </div>
             </div>
         </section>
