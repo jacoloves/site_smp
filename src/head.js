5015e5b61179f3b303ce7c4b5ded1369249d579c
jacoloves <5511068t@gmail.com>
2020-09-27 21:54:26 +0900

    20200927

--- src/head.js
+++ src/head.js
@@ -2,38 +2,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import './style.css';
 
-import Icon from './images/product-logo.png'
-import Icon2 from './images/user.png'
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 const HeadMain = () => {
 
     return(
         <header>
-            <div>
-                <img src={Icon}  alt="logo" class='logo'/>
+            <div class="header-logo">
             </div>
-            {/* <a href="#" onclick="console.log('The link was clicked.'); return false" class='clickme'> 
+            <a href="#" onclick="console.log('The link was clicked.'); return false" class='clickme-pink'> 
                 <div>click</div>
                 <div>me</div>
-            </a> */}
-            <nav>
-                <div class="menu">
-                    <ul class="float">
-                        <li class="first">
-                            <a href="#" onclick="console.log('The link was clicked.'); return false" class='clickme'> 
-                                <div>click</div>
-                                <div>me</div>
-                            </a>
-                        </li>
-                        <li class="second">
-                            <a href="#" onclick="console.log('The link was clicked.'); return false" class='login'> 
-                                <img src={Icon2}/>
-                                <div>login</div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            </a>
+            <a href="#" onclick="console.log('The link was clicked.'); return false" class='click-login'> 
+                <LockOpenIcon fontSize="medium"/>
+                <div>login</div>
+            </a>
         </header>
     );
 }
