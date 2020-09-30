aa115b97e148563cb2eded7607d820dbf380e0f5
jacoloves <5511068t@gmail.com>
2020-09-30 14:48:24 +0900

    20200930

--- src/container.js
+++ src/container.js
@@ -3,6 +3,7 @@ import React from 'react';
 import TopSection from './firstsection'
 import SecSection from './secondsection'
 import ThirdSection from './thirdsection'
+import './style.css';
 
 const ContainerMain = () => {
 
